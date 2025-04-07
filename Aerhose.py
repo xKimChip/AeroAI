de5e2db8a3868e54fc5e43c9b87b091b72ed4cca
RoyceChipman <rchipman@uci.edu>
2025-04-07 00:47:43 -0700

    Simple UI

---
+++ Aerhose.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+
+import json, socket, ssl, sys, time, zlib, math
+import tkinter as tk
+
+
+username = ""
+apikey = ""
+latitude = ""
+longitude = ""
+
+compression = None        # set to "deflate", "decompress", or "gzip" to enable compression
+servername = "firehose.flightaware.com"
+filename = "data/hose_data.json"            # file to save the data
+TO_FILE = True                    # set to True to save the data to a file
+
+def get_user_input():
+   global username, apikey, latitude, longitude
+
+   root = tk.Tk()
+   root.geometry("200x400")
+   root.title("AeroHose")
+   
+   username_var = tk.StringVar(root, value="XXXXX")
+   apikey_var = tk.StringVar(root, value="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+   latitude_var = tk.StringVar(root, value="XX.XXXXXX")
+   longitude_var = tk.StringVar(root, value="XX.XXXXXX")
+   
+   values = {}
+   
+   def submit_values():
+      # Get values from entry fields
+      values['username'] = username_var.get()
+      values['apikey'] = apikey_var.get()
+      values['latitude'] = latitude_var.get()
+      values['longitude']= longitude_var.get()
+
+      # Print values for debugging
+      print(f"Username: {username}")
+      print(f"API Key: {apikey}")
+      print(f"Latitude: {latitude}")
+      print(f"Longitude: {longitude}")
+      
+      root.destroy()  # Close the GUI after submission
+   
+
+   label = tk.Label(root, text="AeroHose Location Tool", font=("Helvetica", 10))
+   label.pack(padx=20, pady=20)
+
+   # Username field
+   user_label = tk.Label(root, text="Username", font=("Helvetica", 8))
+   user_label.pack(padx=10, pady=0)
+   user = tk.Entry(root, width=20, textvariable=username_var)
+   user.pack(padx=10, pady=5)
+
+   # API Key field
+   passkey_label = tk.Label(root, text="API Key", font=("Helvetica", 8))
+   passkey_label.pack(padx=10, pady=0)
+   passkey = tk.Entry(root, width=20, textvariable=apikey_var)
+   passkey.pack(padx=10, pady=5)
+
+   # Latitude field
+   latitude_label = tk.Label(root, text="Latitude", font=("Helvetica", 8))
+   latitude_label.pack(padx=10, pady=0)
+   lat = tk.Entry(root, textvariable=latitude_var)
+   lat.pack(padx=10, pady=5)
+
+   # Longitude field
+   longitude_label = tk.Label(root, text="Longitude", font=("Helvetica", 8))
+   longitude_label.pack(padx=10, pady=0)
+   lon = tk.Entry(root, textvariable=longitude_var)
+   lon.pack(padx=10, pady=5)
+
+
+   # Submit button
+   submit_button = tk.Button(root, text="Submit", command=submit_values,)
+   submit_button.pack(pady=10)
+   
+   root.mainloop()
+   
+   return values['username'], values['apikey'], values['latitude'], values['longitude']
+
+   
+
+class InflateStream:
+   "A wrapper for a socket carrying compressed data that does streaming decompression"
+
+   def __init__(self, sock, mode):
+      self.sock = sock
+      self._buf = bytearray()
+      self._eof = False
+      if mode == 'deflate':     # no header, raw deflate stream
+         self._z = zlib.decompressobj(-zlib.MAX_WBITS)
+      elif mode == 'compress':  # zlib header
+         self._z = zlib.decompressobj(zlib.MAX_WBITS)
+      elif mode == 'gzip':      # gzip header
+         self._z = zlib.decompressobj(16 | zlib.MAX_WBITS)
+      else:
+         raise ValueError('unrecognized compression mode')
+
+   def _fill(self):
+      rawdata = self.sock.recv(8192)
+      if len(rawdata) == 0:
+         self._buf += self._z.flush()
+         self._eof = True
+      else:
+         self._buf += self._z.decompress(rawdata)
+
+   def readline(self):
+      newline = self._buf.find(b'\n')
+      while newline < 0 and not self._eof:
+         self._fill()
+         newline = self._buf.find(b'\n')
+
+      if newline >= 0:
+         rawline = self._buf[:newline+1]
+         del self._buf[:newline+1]
+         return rawline.decode('ascii')
+
+      # EOF
+      return ''
+
+# def haversine(lat1, lon1, lat2, lon2):
+    
+#    R = 6371.0  # Radius of Earth in kilometers
+#    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+    
+#    dlon = lon2 - lon1
+#    dlat = lat2 - lat1
+   
+#    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+#    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+#    distance = R * c
+#    print(f"Distance: {distance:.2f} km")
+#    return distance
+
+# function to parse JSON data:
+def parse_json( str ):
+   try:
+       # parse all data into dictionary decoded:
+       decoded = json.loads(str)
+       
+       
+       # Only looking for positional updates, other types seen "arrival", "flinfo"
+       if decoded["type"] != "position":
+          print(f"Skipped type: {decoded[type]}")
+          return -1
+       #print(haversine(decoded['lat'], decoded['lon'], float(latitude.get()), float(longitude.get())))
+
+      #  elif haversine(decoded['lat'], decoded['lon'], float(latitude.get()), float(longitude.get())) > 0.5:
+      #     print(f"Skipped position: {decoded['lat']}, {decoded['lon']}")
+      #     return -1
+      
+       #print(decoded)
+       if TO_FILE:
+         with open(filename, 'w') as f:
+            json.dump(decoded, f, indent=4)
+       
+
+       # compute the latency of this message:
+       clocknow = time.time()
+       diff = clocknow - int(decoded['pitr'])
+       #print("diff = {0:.2f} s\n".format(diff))
+   except (ValueError, KeyError, TypeError):
+       print("JSON format error: ", sys.exc_info()[0])
+       #print(str)
+       #print(traceback.format_exc())
+   return 0
+
+# get popup for user input
+username, apikey, latitude, longitude = get_user_input()
+
+# Create socket
+sock = socket.socket(socket.AF_INET)
+# Create a SSL context with the recommended security settings for client sockets, including automatic certificate verification
+context = ssl.create_default_context()
+# the folowing line requires Python 3.7+ and OpenSSL 1.1.0g+ to specify minimum_version
+context.minimum_version = ssl.TLSVersion.TLSv1_2
+
+ssl_sock = context.wrap_socket(sock, server_hostname = servername)
+print("Connecting...")
+ssl_sock.connect((servername, 1501))
+print("Connection succeeded")
+
+
+# build the initiation command:
+initiation_command = "live username {} password {}".format(username, apikey)
+if compression is not None:
+   initiation_command += " compression " + compression
+
+# send initialization command to server:
+initiation_command += "\n"
+if sys.version_info[0] >= 3:
+    ssl_sock.write(bytes(initiation_command, 'UTF-8'))
+else:
+    ssl_sock.write(initiation_command)
+
+# return a file object associated with the socket
+if compression is not None:
+   file = InflateStream(sock = ssl_sock, mode = compression)
+else:
+   file = ssl_sock.makefile('r')
+
+
+
+
+# use "while True" for no limit in messages received
+count = 5
+while count > 0:
+   try :
+      # read line from file:
+      inline = file.readline()
+      if inline == '':
+         # EOF
+         break
+
+      # parse the line
+      if parse_json(inline) == 0:
+         count -= 1
+   except socket.error as e:
+      print('Connection fail', e)
+      print(traceback.format_exc())
+
+# wait for user input to end
+# input("\n Press Enter to exit...");
+# close the SSLSocket, will also close the underlying socket
+ssl_sock.close()
+
